3b5f80716ff4f58ef7ddf9fb553e1efb0bba4dbe
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2018-03-23 21:42:01 +0100

    First attempt at responsive

--- js/main.js
+++ js/main.js
@@ -140,6 +140,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = ''; // no need to read image
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
@@ -157,6 +158,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role', 'button');
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
