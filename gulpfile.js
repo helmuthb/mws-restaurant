c1bb3648ff6e30519b557e3567b5efae37364348
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2018-07-31 22:47:47 +0200

    Added critical
    Not really convincing results...

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const imagemin = require('gulp-imagemin');
 const mozjpeg = require('imagemin-mozjpeg');
 const uglify = require('gulp-uglify-es').default;
 const cssnano = require('gulp-cssnano');
+const critical = require('critical');
+const rename = require('gulp-rename');
 
 gulp.task('img', () =>
     gulp.src('src/img/*.jpg')
@@ -20,9 +22,34 @@ gulp.task('js', () =>
         .pipe(gulp.dest('dist/js'))
 );
 gulp.task('files', () =>
-    gulp.src(['src/*.xml', 'src/*.html', 'src/*.png',
-              'src/*.json', 'src/*.js', 'src/*.svg',
-              'src/*.ico'])
+    gulp.src(['src/*.xml', 'src/*.png', 'src/*.json',
+              'src/*.js', 'src/*.svg', 'src/*.ico'])
         .pipe(gulp.dest('dist'))
 );
-gulp.task('default', ['img', 'css', 'js', 'files']);
+gulp.task('html', () =>
+    gulp.src('src/*.html')
+        .pipe(rename({ suffix: '-original' }))
+        .pipe(gulp.dest('dist'))
+);
+gulp.task('build', ['img', 'css', 'js', 'files', 'html']);
+gulp.task('critical', ['build'], () => {
+  critical.generate({
+    inline: true,
+    base: 'dist/',
+    src: 'index-original.html',
+    dest: 'index.html',
+    minify: true,
+    width: 600,
+    height: 800
+  });
+  critical.generate({
+    inline: true,
+    base: 'dist/',
+    src: 'restaurant-original.html',
+    dest: 'restaurant.html',
+    minify: true,
+    width: 600,
+    height: 800
+  });
+});
+gulp.task('default', ['critical']);
